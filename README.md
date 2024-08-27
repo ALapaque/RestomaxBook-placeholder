0dfe6a4552c3d73c3c701b833c97e99e7d5c14f3
alapaque1 <alapaque@easyorderapp.com>
2024-08-27 13:43:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RestomaxBook-placeholder
---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.RestomaxBook = {
+  storeId: 2
+};
+(function () {
+  var script = document.createElement('script');
+  script.id = 'restomaxbook-widget';
+  script.src = 'https://restomax-book.vercel.app/apps/widget/assets/widget.js';
+  document.head.appendChild(script);
+})();
