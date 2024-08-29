c350c0ceb022b441b3b54dcefabb1cd938cff75c
alapaque1 <alapaque@easyorderapp.com>
2024-08-29 16:16:44 +0200

    clean up repo

--- js/app.js
+++ js/app.js
@@ -4,6 +4,6 @@ window.RestomaxBook = {
 (function () {
   var script = document.createElement('script');
   script.id = 'restomaxbook-widget';
-  script.src = 'https://restomax-book.vercel.app/apps/widget/assets/widget.js';
+  script.src = 'https://restomax-book-widget.vercel.app/apps/widget/assets/widget.js';
   document.head.appendChild(script);
 })();
